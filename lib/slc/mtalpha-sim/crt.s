7c84b78a69d19ba6d728ed151271c0c176efff0a
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-02-21 04:24:33 +0000

    slc: import yet more stuff from utcc

---
+++ lib/slc/mtalpha-sim/crt.s
@@ -0,0 +1,25 @@
+	.ent _start
+	.globl _start
+_start:
+	.registers 0 0 31 0 0 0
+	ldah $29, 0($27)	!gpdisp!1
+	lda  $29, 0($29)	!gpdisp!1
+
+	# we want to create t_main
+	# $1-$3 = shareds
+	# $4-$9 = globals
+	# $0 = sync
+	allocate $0, 4, 1, 0, 0
+	
+	ldq $9, t_main($29) !literal
+	crei $0, 0($9)
+	swch
+	mov $31, $1
+	mov $31, $2
+	mov $31, $3
+	
+	# sync, terminate
+	mov $0, $31
+	end	
+	.end _start
+	
