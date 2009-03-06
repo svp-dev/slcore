7da2d4d1319c3b701acc14db378d256c16687c1f
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-03-06 01:00:46 +0000

    Start implementing ppp "compiler".
    * lib/t-pppalpha/slimpl.m4: Add.
    * lib/t-pppalpha/include/sl_support.h: Add.
    * lib/Makefile.am (nobase_dist_pkgdata_DATA): Update accordingly.
    
    * bin/spp: Add.
    * bin/Makefile.am (dist_bin_SCRIPTS): Update accordingly.

---
+++ lib/t-pppalpha/include/sl_support.h
@@ -0,0 +1,135 @@
+#ifndef SL_SUPPORT_H
+#define SL_SUPPORT_H
+
+#define __sl_getshp(Name)						\
+  ({									\
+    __asm__ __volatile__("# MT: read shared " # Name " (%0)" : : "rf"(__sl_shparm_in_ ## Name)); \
+    __sl_shparm_in_ # Name;						\
+  })
+#define __sl_setshp(Name, Value)					\
+  ({									\
+    __typeof__(Value) __tmp_setsh = (Value);				\
+    __asm__ __volatile__("# MT: start write shared " # Name " (%1) <- " # Value " (and clobber incoming %0)" \
+			 : /* ensure incoming shared is marked clobbered */ \
+			   "=rf" (__sl_shparm_in_ ## Name)		\
+			 : "rf"(__sl_shparm_out_ ## Name));		\
+    __sl_shparm_out_ ## Name = __tmp_setsh;				\
+    __asm__ __volatile__("# MT: end write shared " # Name " (%0)"	\
+			 : "=rf" (__sl_shparm_out_ ## Name)		\
+			 : "0"(__sl_shparm_out_ ## Name));		\
+  })
+
+#define __sl_declindex(Name) register const long long Name = __sl_index
+
+#define __sl_prologue(Name, GI, SI, GF, SF, GPREG, IDXREG, IPVREG, ITLSREG, PVREG, TLSREG, LN) \
+  extern void Name(void);						\
+  void _x_ ## Name (void) {						\
+    register long long __sl_index __asm__(IDXREG);			\
+    __asm__ ("# MT: index starts in %0 (must be =" IDXREG ")" : "=r"(__sl_index)); \
+    __asm__ __volatile__(".end _x_" #Name "\n"			\
+			 "\n"						\
+			 "\t.globl " #Name "\n"				\
+			 "\t.ent " #Name "\n"				\
+			 #Name ":\n"					\
+			 "\t.registers " # GI " " # SI " 19 " # GF " " # SF " 19\n" \
+			 "\tldah " GPREG ",0(" IPVREG ")\t!gpdisp!" # LN "\n" \
+			 "\tlda " GPREG ",0(" GPREG ")\t!gpdisp!" # LN "\n" \
+			 "$" # Name "..ng:\n"				\
+			 "\tmov " IPVREG "," PVREG " # PV <- Gx (call convention)\n"	\
+			 "\tmov " ITLSREG "," TLSREG " # SP <- L1 (thread init)\n" \
+			 "\t.prologue 1\n");				\
+
+#define __sl_epilogue(Name, ArgSpec)						\
+  __asm__ __volatile__("# MT: end of thread " #Name "()\n"		\
+		       "\tnop\n"					\
+		       "\tend\n"					\
+		       "# RENAME_END " # Name " " ArgSpec);		\
+  }									
+
+#define __sl_allocate(Tag, GI, SI, GF, SF)				\
+  register long long __sl_fid_ ## Tag;					\
+  __asm__ __volatile__ ("allocate %0, %1, %2, %3, %4\t# CREATE " #Tag \
+			: "=r"(__sl__fid_ ## Tag)		      \
+			: "I"(GI), "I"(SI), "I"(GF), "I"(SF))
+
+#define __sl_setstart(Tag, Start) \
+  __asm__ ("setstart %0, %2\t# CREATE " #Tag		\
+	   : "=r"(__sl_fid_ ## Tag)			\
+	   : "0"(__sl_fid_ ## Tag), "rI"(Start))
+
+#define __sl_setlimit(Tag, Limit) \
+  __asm__ ("setlimit %0, %2\t# CREATE " #Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag), "rI"(Limit))
+
+#define __sl_setplace(Tag, Place) \
+  __asm__ ("setplace %0, %2\t# CREATE " #Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag), "rI"(Place))
+
+#define __sl_setstep(Tag, Step) \
+  __asm__ ("setstep %0, %2\t# CREATE " #Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag), "rI"(Step))
+
+#define __sl_setblock(Tag, Block)					\
+  __asm__ ("setblock %0, %2\t# CREATE " #Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag), "rI"(Block))
+
+#define __sl_dobreak(Val) do {			    \
+    __asm__ __volatile__("break %0" : : "rI"(Val)); \
+    while(1) __nop();                               \
+  } while(0)
+
+#define __sl_dobreakf(Val) do {			    \
+    __asm__ __volatile__("breakf %0" : : "r"(Val)); \
+    while(1) __nop();                               \
+  } while(0)
+
+#define __sl_dokill(Val) __asm__ __volatile__("kill %0" : : "r"(Val))
+
+#define __sl_declshp(Type, Name, Reg1, Reg2) \
+  register Type const __sl_shparm_in_ ## Name __asm__(Reg1); \
+  register Type const __sl_shparm_out_ ## Name __asm__(Reg2)
+
+#define __sl_declglp(Type, Name, Reg) \
+  register Type const __sl_glparm_ ## Name __asm__(Reg)
+
+#define __sl_declgla_noreg(Tag, Type, Name, Init)		\
+  register Type const __sl_glarg_ ## Tag ## __ ## Name = (Init)
+
+#define __sl_declgla(Tag, Type, Name, Reg, Init)			\
+  register Type const __sl_glarg_ ## Tag ## __ ## Name __asm__(Reg) = (Init)
+
+#define __sl_declsha_empty(Tag, Type, Name, Reg) \
+  register Type __sl_sharg_ ## Tag ## __ ## Name __asm__(Reg); \
+  __asm__ ("setempty %0 # EMPTY " # Name : "=r"(__sl_sharg_ ## Tag ## __ ## Name))
+
+#define __sl_declsha(Tag, Type, Name, Reg, Init)		\
+  register Type __sl_sharg_ ## Tag ## __ ## Name __asm__(Reg) = (Init)
+
+#define __sl_setbreak(Tag, BRI) \
+  __asm__ ("setbreak %0, %1\t# CREATE " # Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag) : "I"(BRI))
+
+#define __sl_setbreakf(Tag, BRI) \
+  __asm__ ("setbreakf %0, %1\t# CREATE " # Tag \
+	   : "=r"(__sl_fid_ ## Tag) \
+	   : "0"(__sl_fid_ ## Tag) : "I"(BRI))
+
+#define __sl_declbr(Tag, Type, Reg)		\
+  register Type const __sl_br_ ## Tag __asm__(Reg)
+
+typedef long long sl_family_t;
+typedef long long sl_place_t;
+typedef void (*__sl_fptr_t)(void);
+
+#define SVP_LOCAL 0
+
+#define SVP_ENOERR 0
+#define SVP_EBROKEN 1
+#define SVP_EKILLED 2
+
+#endif
---
+++ lib/t-pppalpha/slimpl.m4
@@ -0,0 +1,72 @@
+###############################################
+# Macro definitions for the PPP-Alpha compiler
+###############################################
+
+m4_define([[__sl_dispcount]],0)
+m4_define([[__sl_crcnt]],0)
+
+m4_define([[sl_decl]], [[void [[$1]](void)]])
+
+m4_define([[sl_shparm]],[[sh:i:[[$1]]:[[$2]]]])
+m4_define([[sl_glparm]],[[gl:i:[[$1]]:[[$2]]]])
+m4_define([[sl_shfparm]],[[sh:f:[[$1]]:[[$2]]]])
+m4_define([[sl_glfparm]],[[gl:f:[[$1]]:[[$2]]]])
+
+m4_define([[sl_def]],[[
+m4_define([[__sl_funcname]], [[$1]])
+m4_define([[__sl_breaktype]], [[$2]])
+m4_step([[__sl_dispcount]])
+m4_define([[__sl_thparms]],"m4_join([[" "]],m4_shiftn(2,$@))")
+m4_esyscmd([[../../bin/spp pppalpha fundef ]]__sl_funcname __sl_dispcount __sl_thparms)
+]])
+m4_define([[sl_enddef]],[[
+m4_foreach([[__sl_parmname]],m4_quote(__sl_parmnames),[[
+m4_undefine([[__sl_getp_]]__sl_parmname)
+m4_undefine([[__sl_setp_]]__sl_parmname)
+]])
+__sl_epilogue(__sl_funcname, __sl_parmspec)
+]])
+
+m4_define([[sl_getp]],[[__sl_getp_$1]])
+m4_define([[sl_setp]],[[__sl_setp_$1([[$2]])]])
+
+m4_define([[sl_sharg]],[[sh:i:[[$1]]:[[$2]]:[[$3]]]])
+m4_define([[sl_glarg]],[[gl:i:[[$1]]:[[$2]]:[[$3]]]])
+m4_define([[sl_shfarg]],[[sh:f:[[$1]]:[[$2]]:[[$3]]]])
+m4_define([[sl_glfarg]],[[gl:f:[[$1]]:[[$2]]:[[$3]]]])
+
+m4_define([[sl_create]],[[
+m4_step([[__sl_crcnt]])
+m4_define([[__sl_autotag]],__child[[]]__sl_crcnt[[]]__)
+m4_define([[__sl_tag]],m4_if([[$1]],,__sl_autotag,[[$1]]))
+m4_define([[_sl_place]],m4_if([[$2]],,[[SVP_LOCAL]],[[$2]]))
+m4_define([[_sl_start]],m4_if([[$3]],,0,[[$3]]))
+m4_define([[_sl_limit]],m4_if([[$4]],,1,[[$4]]))
+m4_define([[_sl_step]],m4_if([[$5]],,1,[[$5]]))
+m4_define([[_sl_block]],m4_if([[$6]],,0,[[$6]]))
+m4_define([[__sl_crbrktype]],"[[$7]]")
+m4_define([[__sl_crfuncname]],"[[$8]]")
+m4_define([[__sl_thargs]],"m4_join([[" "]],m4_shiftn(8,$@))")
+m4_esyscmd([[../../bin/spp pppalpha create ]]__sl_crfuncname __sl_tag __sl_crbrktype __sl_thargs)
+__sl_setplace(__sl_tag, _sl_place);
+__sl_setstart(__sl_tag, _sl_start);
+__sl_setlimit(__sl_tag, _sl_limit);
+__sl_setstep(__sl_tag, _sl_step);
+__sl_setblock(__sl_tag, _sl_block);
+]])
+
+m4_define([[sl_break]],[[
+m4_if(__sl_breaktype,
+int,[[__sl_dobreak([[$1]])]],
+float,[[__sl_dobreakf([[$1]])]],
+,[[
+#error cannot break in this function
+]])
+]])
+
+m4_define([[sl_kill]],[[__sl_dokill([[$1]])]])
+
+m4_define([[sl_index]],[[__sl_declindex([[$1]])]])
+
+
+
