7400beb31ba12cf3308f4e72ccd39550973046eb
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-03-22 14:10:24 +0000

    Reinstate "make distcheck" by hiding the unit test suite behind a configure argument.
    * configure.ac: Add conditional "USE_TESTSUITE".
    * tests/mkalltests.sh,	
    * tests/Makefile.am: Activate unit test suite only when
    USE_TESTSUITE is enabled.
    
    * doc/HACKING.txt: Document the change.
    
    * bin/slt: Clean up better after a test succeeds, especially the
    directory containing the reference output.
    
    * Makefile.am: Add missing reference to include/svp/roman.slh.

--- tests/mkalltests.sh
+++ tests/mkalltests.sh
@@ -3,7 +3,7 @@ root=$(dirname "$0")
 cd "$root"
 rm -f alltests.mk
 exec >alltests.mk
-echo 'TESTS = ' 
-find . -name \*.sl | grep -v '/x_' | sed -e 's/^\.\/\(.*\)$/TESTS += \1/g'
+echo 'TESTSUITE = ' 
+find . -name \*.sl | grep -v '/x_' | sed -e 's/^\.\/\(.*\)$/TESTSUITE += \1/g'
 chmod -w alltests.mk
 
