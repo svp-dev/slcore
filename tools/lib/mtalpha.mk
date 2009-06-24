608c515ff814398976cc2e78a6fc90294adae9dc
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-06-24 07:05:13 +0000

    * programs/benchmarks/nas/Makefile.am * programs/benchmarks/nas/is/Makefile.am * programs/benchmarks/blas/Makefile.am * programs/benchmarks/quicksort/Makefile.am * tools/lib/mtalpha.mk * tools/lib/seqc.mk * tools/lib/ptl.mk * tools/lib/Makefile.am * tools/Makefile.am: Add files missing from previous commit.

---
+++ tools/lib/mtalpha.mk
@@ -0,0 +1,42 @@
+########### MT-Alpha components ###########
+
+EXTRA_DIST += \
+	mtalpha-sim/slrt.s \
+	mtalpha-sim/callgate.s
+
+nobase_dist_pkgdata_DATA += \
+	t-pppalpha/slimpl.m4 \
+	t-pppalpha/include/sl_support.h
+
+if ENABLE_MTALPHA
+
+nobase_pkglib_DATA += mtalpha-sim/slrt.o
+
+nobase_pkglib_LIBRARIES += mtalpha-sim/libslc.a
+
+
+mtalpha_sim_libslc_a_SOURCES = 
+mtalpha_sim_libslc_a_CONTENTS = \
+	mtalpha-sim/callgate.o 
+
+mtalpha-sim/libslc.a: $(mtalpha_sim_libslc_a_CONTENTS)
+	$(AM_V_at)rm -f $@
+	$(AM_V_AR)$(AR_MTALPHA) cru $@ $^
+	$(AM_V_at)$(RANLIB_MTALPHA) $@
+
+SLC_MTALPHA = $(SLC) -b ppp-mtalpha -nostdlib
+
+mtalpha-sim/callgate.o: $(srcdir)/mtalpha-sim/callgate.s
+	$(AM_V_at)$(MKDIR_P) mtalpha-sim
+	$(slc_verbose)$(SLC_MTALPHA) -c -o $@ $<
+
+mtalpha-sim/slrt.o: $(srcdir)/mtalpha-sim/slrt.s
+	$(AM_V_at)$(MKDIR_P) mtalpha-sim
+	$(slc_verbose)$(SLC_MTALPHA) -c -o $@ $<
+
+endif
+
+CLEANFILES += \
+	mtalpha-sim/callgate.o \
+	mtalpha-sim/slrt.o \
+	mtalpha-sim/libslc.a
---
+++ tools/lib/seqc.mk
@@ -0,0 +1,18 @@
+########## Sequential-C implementation ##########
+
+nobase_dist_pkgdata_DATA += \
+	t-seqc/slimpl.m4 \
+	t-seqc/include/sl_support.h
+
+EXTRA_DIST += host-host-seqc/slrt.c host-host-seqc/load.c
+
+nobase_pkglib_DATA += host-host-seqc/slrt.o
+
+SLC_SEQC = $(SLC) -b seqc -nostdlib $(AM_CFLAGS) $(CFLAGS)
+
+host-host-seqc/slrt.o: $(srcdir)/host-host-seqc/slrt.c $(srcdir)/host-host-seqc/load.c
+	$(AM_V_at)$(MKDIR_P) host-host-seqc
+	$(slc_verbose)$(CC) $(CFLAGS) -I$(srcdir)/t-seqc/include -c -o $@ $< $(AM_CFLAGS) $(CFLAGS)
+
+CLEANFILES += \
+	host-host-seqc/slrt.o
