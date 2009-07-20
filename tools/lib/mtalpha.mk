6ae8ebf4f17aaec64b97aba2514b469728d3cd4b
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-07-20 12:48:47 +0000

    Fix linking against `mtalpha-sim/libslc.a' after the toolchain is installed.
    * tools/lib/mtalpha.mk: Pull libslc.a out of pkgdata_LIBRARIES,
    because otherwise Automake causes the host's "ranlib" utility to
    be run on the archive during installation. Use the DATA target
    instead.

--- tools/lib/mtalpha.mk
+++ tools/lib/mtalpha.mk
@@ -10,12 +10,10 @@ nobase_dist_pkgdata_DATA += \
 
 if ENABLE_MTALPHA
 
-nobase_pkglib_DATA += mtalpha-sim/slrt.o
+nobase_pkglib_DATA += \
+	mtalpha-sim/slrt.o \
+	mtalpha-sim/libslc.a
 
-nobase_pkglib_LIBRARIES += mtalpha-sim/libslc.a
-
-
-mtalpha_sim_libslc_a_SOURCES = 
 mtalpha_sim_libslc_a_CONTENTS = \
 	mtalpha-sim/callgate.o 
 
@@ -34,9 +32,10 @@ mtalpha-sim/slrt.o: $(srcdir)/mtalpha-sim/slrt.s
 	$(AM_V_at)$(MKDIR_P) mtalpha-sim
 	$(slc_verbose)$(SLC_MTALPHA) -c -o $@ $<
 
-endif
-
 CLEANFILES += \
 	mtalpha-sim/callgate.o \
 	mtalpha-sim/slrt.o \
 	mtalpha-sim/libslc.a
+
+endif
+
