cb119d1b5d4fc97b9fc39a914ee3ce3497892bb5
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-07-21 13:58:10 +0000

    Move forward with `make check' on LL benchmark code.
    * programs/benchmarks/livermore/ll.slh: Remove structure
    definition (put arrays in global scope) to prevent the compiler
    from complaining the structure is too large. Adjust LARGEST to
    please the linker on MacOS X.
    	
    * programs/benchmarks/livermore/bench1.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench3.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench4.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench5.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench7.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench9.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench10.bin.mtalpha.psize,
    * programs/benchmarks/livermore/bench11.bin.mtalpha.psize,	
    * programs/benchmarks/livermore/bench12.bin.mtalpha.psize: Adapt
    problem size to new value of LARGEST.
    	
    * programs/benchmarks/livermore/gencode.sh: Indicate to `slt' that
    the `bench' programs take Fibre input. Generate the Fibre data
    from the `.psize' files and distribute it. Indicate to `slt' that
    the output of the `test' programs should not be compared to the
    reference. Cleanup generated files on `make maintainerclean'
    
    * programs/benchmarks/livermore/Makefile.am (MAINTAINERCLEANFILES):
    Initialize variable so that `ll.mk' can append to it.

--- programs/benchmarks/livermore/gencode.sh
+++ programs/benchmarks/livermore/gencode.sh
@@ -2,14 +2,26 @@
 echo >"ll.mk"
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
     cat >"bench$i.sl" <<EOF
+// SLT_RUN: -f TEST.in
 m4_define([[KERNEL]], $i)
 m4_include(bench.sl)
 EOF
+    (
+	echo 1;  # number of iterations
+	if test -r "bench$i.bin.mtalpha.psize"; then 
+	    cat "bench$i.bin.mtalpha.psize"
+	else 
+	    echo 1000
+	fi
+	) >"bench$i.sl.in"
+    echo "EXTRA_DIST += bench$i.sl.in" >>"ll.mk"
     cat >"test$i.sl" <<EOF
+// XIGNORE: *:D
 m4_define([[KERNEL]], $i)
 m4_include(test.sl)
 EOF
     echo "DEMOS += bench$i.sl test$i.sl" >>"ll.mk"
+    echo "MAINTAINERCLEANFILES += bench$i.sl test$i.sl bench$i.sl.in" >>"ll.mk"
 done
 for f in *.psize; do
     echo "EXTRA_DIST += $f" >>"ll.mk"
