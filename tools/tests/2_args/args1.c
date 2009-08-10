0dd373a1412aaeb2c70f15a95c9844d0a13c24c3
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-08-10 16:50:03 +0000

    Continue removing dependency on `lib/' from `tools/'
    * tools/tests/2_args/args1.sl: Rename to...
    * tools/tests/2_args/args1-old.sl: ... here, and copy to...
    * tools/tests/2_args/args1.c: ...here. Use new header style, drop
    dependency on `svp_assert' and use `svp_abort' from
    `<svp/abort.h>' directly.

---
+++ tools/tests/2_args/args1.c
@@ -0,0 +1,39 @@
+//
+// args1.sl: this file is part of the slc project.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// $Id$
+//
+
+#include <svp/abort.h>
+
+sl_def(foo, void, sl_shparm(int, a))
+{
+  sl_setp(a, sl_getp(a) + 1);
+}
+sl_enddef
+
+sl_def(t_main, void)
+{
+  sl_create(,,,,,,, foo, sl_sharg(int, a));
+  sl_seta(a, 10);
+  sl_sync();
+  if (sl_geta(a) != 11) svp_abort();
+  {
+    sl_create(,,,,,,, foo, sl_sharg(int, a));
+    sl_seta(a, 20);
+    sl_sync();
+    if (sl_geta(a) != 21) svp_abort();
+  }
+  if (sl_geta(a) != 11) svp_abort();
+}
+sl_enddef
