d803dde1c682226d9fc1b87d2012ebf0ee3a6667
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-08-10 16:49:46 +0000

    Refactor headers in `tools/include'. Update test suite accordingly.
    * tools/include/fibre.h,
    * tools/include/slr.h: Rename to...
    * tools/include/svp/fibre.h,
    * tools/include/svp/slr.h: ... here.
    
    * tools/include/slr.slh,
    * tools/include/fibre.slh: Update accordingly.
    
    * tools/include/svp/lib.slh: Copy to...
    * tools/include/svp/compiler.h: here. Use CPP syntax more. Define
    `nop' locally. Move `svp_abort' definition to...
    
    * tools/include/svp/abort.h: here.
    
    * tools/include/svp/abort.h,
    * tools/include/svp/compiler.h: Warn if `__GNUC__' is not defined.
    
    * tools/include/svp/lib.slh: Deprecate.
    
    * tools/tests/0_trivial/x_abort.sl,
    * tools/tests/0_trivial/nop.sl,
    * tools/tests/1_create/crnop.sl,
    * tools/tests/1_create/crnop2.sl,
    * tools/tests/1_create/crnop3.sl,
    * tools/tests/1_create/crnopdefs.sl,
    * tools/tests/1_create/x_inffamily.sl,
    * tools/tests/2_args/delayshared.sl,
    * tools/tests/2_args/globpass.sl,
    * tools/tests/2_args/useexit2.sl: Rename to...
    
    * tools/tests/0_trivial/x_abort-old.sl,
    * tools/tests/0_trivial/nop-old.sl,
    * tools/tests/1_create/crnop-old.sl,
    * tools/tests/1_create/crnop2-old.sl,
    * tools/tests/1_create/crnop3-old.sl,
    * tools/tests/1_create/crnopdefs-old.sl,
    * tools/tests/1_create/x_inffamily-old.sl,
    * tools/tests/2_args/delayshared-old.sl,
    * tools/tests/2_args/globpass-old.sl,
    * tools/tests/2_args/useexit2-old.sl: ... here, and copy to...
    
    * tools/tests/0_trivial/x_abort.c,
    * tools/tests/0_trivial/nop.c,
    * tools/tests/1_create/crnop.c,
    * tools/tests/1_create/crnop2.c,
    * tools/tests/1_create/crnop3.c,
    * tools/tests/1_create/crnopdefs.c,
    * tools/tests/1_create/x_inffamily.c,
    * tools/tests/2_args/delayshared.c,
    * tools/tests/2_args/globpass.c,
    * tools/tests/2_args/useexit2.c: ... here. Use new headers.
    
    * tests.mk: Also recognize .c as a unit test extension. Define
    `C_LOG_COMPILER' the same as `SL_LOG_COMPILER'.
    
    * tools/tests/mkalltests.sh: Pull .c files into the test suite,
    but exclude `.t.' temporaries.

---
+++ tools/include/svp/compiler.h
@@ -0,0 +1,46 @@
+//
+// compiler.h: this file is part of the slc project.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// $Id$
+//
+#ifndef __SVP_COMPILER_H__
+# define __SVP_COMPILER_H__
+
+#ifndef __GNUC__
+# warn __GNUC__ is not defined. Some macros here may not work.
+#endif
+
+/* The following code is inspired from <linux/compiler.h> */
+
+/* Definition for a memory barrier primitive */
+#define barrier() __asm__ __volatile__("" : : : "memory")
+
+#ifdef __INTEL_COMPILER
+# undef barrier
+# define barrier() __memory_barrier()
+#endif
+
+/* Branch optimization */
+
+#define likely(X) __builtin_expect(!!(X), 1)
+#define unlikely(X) __builtin_expect(!!(X), 0)
+
+/* No operation instruction */
+
+#ifdef __mt_freestanding__
+# define nop() __asm__ __volatile__("nop\n\tswch");
+#else
+# define nop() __asm__ __volatile__("nop");
+#endif
+
+#endif
---
+++ tools/tests/2_args/delayshared.c
@@ -0,0 +1,51 @@
+//
+// delayshared.sl: this file is part of the slc project.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+// $Id$
+//
+
+#include <svp/compiler.h>
+#include <svp/iomacros.slh>
+
+sl_def(foo, void, sl_shparm(int, a))
+{
+  sl_setp(a, sl_getp(a) + 1);
+}
+sl_enddef
+
+// 2009-04-02: FIXME: we acknowledge that muTC-ptl
+// does not support this construct fully yet; but
+// we want slc's testsuite to properly succeed. So
+// we mark the test to ignore the output on muTC-ptl:
+// XIGNORE: ptl*:D
+
+sl_def(t_main, void)
+{
+  int busy;
+  sl_create(,,,,,,,
+	    foo, sl_sharg(int, a));
+  // FIXME: for ptl here we should find a way to
+  // force a context switch, otherwise the problem
+  // is not demonstrated. In the previous version
+  // of this test, a call was done to the C library's
+  // putc() function, but we can't do this in SL
+  // because putc is a thread function and we cannot
+  // nest creates.
+  for (busy = 0; busy < 10000; ++busy) nop();
+  sl_seta(a, 42);
+  sl_sync();
+
+  putn(sl_geta(a));
+  putc('\n');
+}
+sl_enddef
