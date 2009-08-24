ef7fecf055ad917ba0e0d2429e621dd1586d5f63
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-08-24 15:48:01 +0000

    Make call gate more robust on MT-Alpha.
    When a function using varargs is being called, it will first try
    to push all the argument registers to the stack upon entry. This
    fails if only some where initialized by the caller (in our case,
    the call gate).
    
    * tools/lib/mtalpha-sim/slrt.s,
    * tools/lib/mtalpha-sim/callgate.s: Ensure all call argument
    registers are not empty.
    
    * tools/bin/spp,
    * tools/lib/mtalpha-sim/slrt.s,
    * tools/lib/mtalpha-sim/callgate.s: Improve readability by using
    `clr' and `fclr' instead of `mov $31' and `cpys $f31'.

--- tools/lib/mtalpha-sim/callgate.s
+++ tools/lib/mtalpha-sim/callgate.s
@@ -6,59 +6,77 @@
 	.ent __sl_callgate
 __sl_callgate:
 	.registers 0 0 31 0 0 31
-	mov $l0, $30   # SP(parent)
-	ldfp $l1       # TLSP
+	mov $0, $30   # SP(parent)
+	ldfp $1       # TLSP
 
 	ldq $9, -8($30)  # load call protocol
 	ldq $27, -16($30) # load PV
 
 	mov $30, $15   # set up frame pointer
+	clr $10 # flush callee-save reg
+	clr $11 # flush callee-save reg
+	clr $12 # flush callee-save reg
+	clr $13 # flush callee-save reg
+	clr $14 # flush callee-save reg
+	fclr $f2 # flush callee-save reg
+	fclr $f3 # flush callee-save reg
+	fclr $f4 # flush callee-save reg
+	fclr $f5 # flush callee-save reg
+	fclr $f6 # flush callee-save reg
+	fclr $f7 # flush callee-save reg
+	fclr $f8 # flush callee-save reg
+	fclr $f9 # flush callee-save reg
+	fclr $f0 # init FP return reg
 
-	beq $9, $docall
+	beq $9, $docall_16
 	ldq $16, -24($0)
 	ldt $f16, -32($0)
 
 	subl $9, 1, $9
-	beq $9, $docall
+	beq $9, $docall_17
 	ldq $17, -40($0)		
 	ldt $f17, -48($0)
 
 	subl $9, 1, $9
-	beq $9, $docall
+	beq $9, $docall_18
 	ldq $18, -56($0)
 	ldt $f18, -64($0)
 
 	subl $9, 1, $9
-	beq $9, $docall
+	beq $9, $docall_19
 	ldq $19, -72($0)
 	ldt $f19, -80($0)
 
 	subl $9, 1, $9
-	beq $9, $docall
+	beq $9, $docall_20
 	ldq $20, -88($0)
 	ldt $f20, -96($0)
 
 	subl $9, 1, $9
-	beq $9, $docall
+	beq $9, $docall_21
 	ldq $21, -104($0)
 	ldt $f21, -112($0)
+	br $docall
 
+$docall_16:
+	clr $16
+	fclr $f16	
+$docall_17:
+	clr $17
+	fclr $f17
+$docall_18:
+	clr $18
+	fclr $f18
+$docall_19:
+	clr $19
+	fclr $f19
+$docall_20:
+	clr $20
+	fclr $f20
+$docall_21:
+	clr $21
+	fclr $f21
 $docall:
-	mov $31, $10 # flush callee-save reg
-	mov $31, $11 # flush callee-save reg
-	mov $31, $12 # flush callee-save reg
-	mov $31, $13 # flush callee-save reg
-	mov $31, $14 # flush callee-save reg
-	cpys $f31, $f31, $f2 # flush callee-save reg
-	cpys $f31, $f31, $f3 # flush callee-save reg
-	cpys $f31, $f31, $f4 # flush callee-save reg
-	cpys $f31, $f31, $f5 # flush callee-save reg
-	cpys $f31, $f31, $f6 # flush callee-save reg
-	cpys $f31, $f31, $f7 # flush callee-save reg
-	cpys $f31, $f31, $f8 # flush callee-save reg
-	cpys $f31, $f31, $f9 # flush callee-save reg
-	cpys $f31, $f31, $f0 # init FP return reg
-
 	# call function
 	jsr $26,($27)
 
