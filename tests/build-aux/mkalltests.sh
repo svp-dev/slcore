d7cb7107556ec40c0b0142e56991e7b1fe23914a
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-08-26 16:38:34 +0200

    [sl-tests] Minor packaging fix.
    * build-aux/mkalltests.sh: Be more verbose.

--- tests/build-aux/mkalltests.sh
+++ tests/build-aux/mkalltests.sh
@@ -5,17 +5,22 @@ root=$(dirname "$0")
 cd "$root"
 rm -f alltests.mk
 
+dst=$srcdir/0_slc/2_manyargs
+echo "Generating tests in $dst..."
 for ns in 1 2 3 4; do
   for ng in 1 2 10 20; do
-    (cd "$srcdir"/0_slc/2_manyargs && python ./gencall.py $ns $ng int >int_${ns}_$ng.c)
-    (cd "$srcdir"/0_slc/2_manyargs && python ./gencall.py $ns $ng float >fp_${ns}_$ng.c)
+    (cd "$dst" && python ./gencall.py $ns $ng int >int_${ns}_$ng.c)
+    (cd "$dst" && python ./gencall.py $ns $ng float >fp_${ns}_$ng.c)
   done
 done
+dst=$srcdir/0_slc/2_manylocals
+echo "Generating tests in $dst..."
 for ns in 1 2 3 40; do
-  (cd "$srcdir"/0_slc/2_manylocals && python ./genmany.py $ns 0 >many$ns.c)
-  (cd "$srcdir"/0_slc/2_manylocals && python ./genmany.py $ns 1 >manyf$ns.c)
+  (cd "$dst" && python ./genmany.py $ns 0 >many$ns.c)
+  (cd "$dst" && python ./genmany.py $ns 1 >manyf$ns.c)
 done
 
+echo "Generating Make rules..."
 exec >alltests.mk
 echo 'TESTSUITE = ' 
 
