8ec5529dd7abc6beac731dd7a10f35ef9b3fa172
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-09-25 08:30:25 +0000

    [sl-tests] Extend the unit test suite.
    The following bugs were found: when C99 VLA's would be allocated,
    or local arrays with large sizes, the compiler would emit invalid
    code due to the stack probing logic emitted by GCC. (Reported by
    Carl Joslin).
    
    * 0_slc/4_vlas/bigarray.c,
    * 0_slc/4_vlas/idxvla.c: New.

---
+++ tests/0_slc/4_vlas/idxvla.c
@@ -0,0 +1,31 @@
+//
+// idxvla.c: this file is part of the SL toolchain.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+
+#include <svp/compiler.h>
+#include <svp/testoutput.h>
+
+noinline
+int foo(int*a) { barrier(); return a[42]; }
+
+sl_def(t_main, void)
+{
+  sl_index(i);
+  int a[i + 69];
+  a[42] = 123;
+  output_int(foo(a), 1);
+  output_char('\n', 1);
+  output_int(a[42], 1);
+  output_char('\n', 1);
+}
+sl_enddef
