0bed2a5a6d33366533aa6fb3ae98559a8603d6fb
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-10-01 09:27:04 +0000

    [sl-programs] Reinstate numeric checking for the FFT code.
    * benchmarks/fft/fft_test1.c,
    * benchmarks/fft/fft_test2.c: Do not ignore numeric output anymore
    since the diverging results on MT-Alpha were due to a simulation
    bug that is now fixed.

--- programs/benchmarks/fft/fft_test1.c
+++ programs/benchmarks/fft/fft_test1.c
@@ -17,12 +17,6 @@
 // SLT_RUN:  M=3 -dPi  Pf=1 -dPc  BR=1
 // SLT_RUN:  M=4 -dPi  Pf=1 -dPc  BR=1
 
-// FIXME: as of 2004-04-05 the output of this
-// program seems implementation dependent;
-// so ignore the diff during testing:
-// XIGNORE: *:D
-
-
 #include "fft_test.c"
 #include "fft_impl1.c"
 #include "fft_extra.c"
