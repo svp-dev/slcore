b0baa479103efa2210e40513b25567b093a946d7
Simon Polstra <s.polstra@uva.nl>
2009-10-13 16:20:30 +0000

    This testcase checks if the core compiler removes the statement: shared = shared

---
+++ tests/0_slc/1_createargs/1_shared.c
@@ -0,0 +1,33 @@
+//
+// 1_shared.c: this file is part of the SL toolchain.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+
+#include <svp/testoutput.h>
+
+sl_def(foo, void, sl_shparm(int, sarg))
+{
+  sl_index(i);
+  if (i == 0) 
+      sl_setp(sarg, sl_getp(sarg)); // should not be removed by core-compiler
+  else 
+      sl_setp(sarg, sl_getp(sarg)+1);
+}
+sl_enddef
+
+sl_def(t_main, void)
+{
+  sl_create(,,0,9,1,,, foo, sl_sharg(int, s_in, 7));
+  sl_sync();
+  output_int(sl_geta(s_in), 1);
+}
+sl_enddef
