cfc488bba63b5ae9320df5996c953431d9d29b7f
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-11-03 17:40:15 +0000

    [sl-programs] Extend the BLAS benchmark suite.
    * benchmarks/blas/templates/xscal.c,
    * benchmarks/blas/sscal.c,
    * benchmarks/blas/dscal.c,
    * benchmarks/blas/bench-sscal.c,
    * benchmarks/blas/bench-dscal.c,
    * benchmarks/blas/bench-sscal.inputs,
    * benchmarks/blas/bench-dscal.inputs: New.
    
    * benchmarks/blas/Makefile.am: Update accordingly.

---
+++ programs/benchmarks/blas/bench-sscal.c
@@ -0,0 +1,116 @@
+//
+// bench-sscal.c: this file is part of the SL toolchain.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+
+#include <svp/testoutput.h>
+#include <svp/perf.h>
+#include <svp/fibre.h>
+#include <svp/assert.h>
+#include <cmalloc.h>
+
+#include "benchmark.h"
+
+struct bdata {
+  long n;
+  float a;
+  float *sx;
+  double *sx_orig;
+};
+
+sl_def(initialize, void,
+       sl_glparm(struct benchmark_state*, st))
+{
+  int i;
+  struct bdata *bdata = (struct bdata*) malloc(sizeof(struct bdata));
+  svp_assert(bdata != NULL);
+
+  /* benchmark input:
+     1 x ulong
+     1 x double
+     1 x double array (X) */
+  svp_assert(fibre_tag(0) == 0);
+  svp_assert(fibre_rank(0) == 0);
+  bdata->n = *(long*)fibre_data(0);
+
+  svp_assert(fibre_tag(1) == 2);
+  svp_assert(fibre_rank(1) == 0);
+  bdata->a = *(double*)fibre_data(1);
+
+  svp_assert(fibre_tag(2) == 2);
+  svp_assert(fibre_rank(2) == 1);
+  svp_assert(fibre_shape(2)[0] >= bdata->n);
+  bdata->sx_orig = (double*)fibre_data(2);
+
+  bdata->sx = (float*)malloc(bdata->n * sizeof(float));
+  svp_assert(bdata->sx != NULL);
+
+  sl_getp(st)->data = (void*) bdata;
+}
+sl_enddef
+
+sl_def(prepare, void,
+       sl_glparm(struct benchmark_state*, st))
+{
+  struct bdata *bdata = (struct bdata*)sl_getp(st)->data;
+  long i;
+  for (i = 0; i < bdata->n; ++i)
+    bdata->sx[i] = bdata->sx_orig[i];
+}
+sl_enddef
+
+sl_def(output, void,
+       sl_glparm(struct benchmark_state*, st))
+{
+  struct bdata *bdata = (struct bdata*)sl_getp(st)->data;
+  long i;
+  for (i = 0; i < bdata->n; ++i) {
+    output_float(bdata->sx[i], 1, 4);
+    output_char('\n', 1);
+  }
+}
+sl_enddef
+
+sl_def(teardown, void,
+       sl_glparm(struct benchmark_state*, st))
+{
+  struct bdata *bdata = (struct bdata*)sl_getp(st)->data;
+  free(bdata->sx);
+  free(bdata);
+}
+sl_enddef
+
+#include "sscal.c"
+
+sl_def(work, void,
+       sl_glparm(struct benchmark_state*, st))
+{
+  struct bdata *bdata = (struct bdata*)sl_getp(st)->data;
+  sl_proccall(sscal,
+	      sl_glarg(long, n, bdata->n),
+	      sl_glfarg(float, a, bdata->a),
+	      sl_glarg(float*, sx, bdata->sx),
+	      sl_glarg(long, incx, 1));
+}
+sl_enddef
+
+sl_def(t_main, void)
+{
+  struct benchmark b = {
+    "BLAS: SSCAL",
+    "kena",
+    "Compute X[i] = A * X[i] with single precision",
+    &initialize, &prepare, &work, &output, &teardown
+  };
+  sl_proccall(run_benchmark, sl_glarg(struct benchmark*, b, &b));
+}
+sl_enddef
---
+++ programs/benchmarks/blas/templates/xscal.c
@@ -0,0 +1,42 @@
+//
+// xscal.c: this file is part of the SL toolchain.
+//
+// Copyright (C) 2009 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+
+#define INT long
+
+sl_def(FUNCTION[[]]_mt, void,
+       sl_glfparm(FLOAT, a),
+       sl_glparm(FLOAT*restrict, sx))
+{
+  sl_index(i);
+  FLOAT *restrict lsx = sl_getp(sx) + i;
+  *lsx = *lsx * sl_getp(a);
+}
+sl_enddef
+
+sl_def(FUNCTION, void,
+       sl_glparm(INT, n),
+       sl_glfparm(FLOAT, a),
+       sl_glparm(FLOAT*, sx),
+       sl_glparm(INT, incx))
+{
+  INT sx = (sl_getp(incx) < 0) ? ((-sl_getp(n) + 1) * sl_getp(incx)) : 0;
+  INT lx = (sl_getp(incx) < 0) ? -1 : sl_getp(n);
+
+  sl_create(,, sx, lx, sl_getp(incx),,,
+	    FUNCTION[[]]_mt,
+	    sl_glfarg(FLOAT, _0, sl_getp(a)),
+	    sl_glarg(FLOAT*, _1, sl_getp(sx)));
+  sl_sync();
+}
+sl_enddef
