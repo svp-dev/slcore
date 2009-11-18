cb2ee454f0d4cf8ed6c819a097b3bb8ff422403b
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-11-18 01:43:41 +0000

    [sl-core] Extend the performance counters API in the library.
    * lib/include/svp/perf.h,
    * lib/src/perf.c: Add the features described in note [sl8].

--- slc/lib/include/svp/perf.h
+++ slc/lib/include/svp/perf.h
@@ -31,8 +31,10 @@ typedef long counter_t;
 #define MTPERF_ISSUED_FP_INSNS 2
 #define MTPERF_NCOUNTERS 3
 typedef struct { counter_t ct[MTPERF_NCOUNTERS]; } __counters_t;
-#define mtperf_sample(Array) do { \
-  *(__counters_t*restrict)(void*)(Array) = *(volatile __counters_t*restrict)(void*)8; \
+#define mtperf_sample(Array) do {					\
+    __asm__ __volatile__("# sample begins");				\
+    *(__counters_t*restrict)(void*)(Array) = *(volatile __counters_t*restrict)(void*)8; \
+    __asm__ __volatile__("# sample ends");				\
   } while(0)
 
 #define mtperf_sample1(Counter) (((volatile __counters_t*restrict)(void*)8)->ct[Counter])
@@ -50,9 +52,48 @@ extern void mtperf_report_diffs(const counter_t *before, const counter_t* after,
 
 #define CSV_SEP(C) ((((int)(C))<<16)|2)
 #define CSV_INCLUDE_HEADER 1
+#define REPORT_NOLF 4
 #define REPORT_CSV 0
 #define REPORT_FIBRE (2<<8)
 #define REPORT_RAW (1<<8)
 #define REPORT_STREAM(N) ((N)<<24)
+
+struct s_interval {
+  counter_t before[MTPERF_NCOUNTERS];
+  counter_t after[MTPERF_NCOUNTERS];
+  const char *tag;
+  int num;
+};
+
+void mtperf_empty_interval(struct s_interval* ivs, size_t p,
+			   int numarg, const char *tag);
+#define mtperf_empty_interval_(Ivs, P, Numarg, Tag) do {	  \
+    struct s_interval __init = { { 0 }, { 0 }, (Tag), (Numarg) }; \
+    (Ivs)[P] = __init;						  \
+  } while(0)
+#define mtperf_empty_interval mtperf_empty_interval_
+
+void mtperf_start_interval(struct s_interval* ivs, size_t p,
+			   int numarg,
+			   const char *tag);
+#define mtperf_start_interval_(Ivs, P, Numarg, Tag) do {		\
+    struct s_interval* __ivs = (Ivs);					\
+    size_t __p = (P);							\
+    __ivs[__p].tag = (Tag);						\
+    __ivs[__p].num = (Numarg);						\
+    mtperf_sample(__ivs[__p].before);					\
+  } while(0)
+#define mtperf_start_interval mtperf_start_interval_
+
+void mtperf_finish_interval(struct s_interval* ivs, size_t p);
+#define mtperf_finish_interval_(Ivs, P) mtperf_sample((Ivs)[P].after)
+#define mtperf_finish_interval mtperf_finish_interval_
+
+void mtperf_report_intervals(const struct s_interval* ivs, size_t n, int flags);
+
+// The following is simply an alias to calloc(n, sizeof(struct s_interval))
+struct s_interval* mtperf_alloc_intervals(size_t n);
+// The following is simply an alias to free(p)
+void mtperf_free_intervals(struct s_interval* p);
 
 #endif // ! SLC_SVP_PERF_H
--- slc/lib/src/perf.c
+++ slc/lib/src/perf.c
@@ -14,6 +14,7 @@
 
 #include <svp/perf.h>
 #include <svp/testoutput.h>
+#include <cmalloc.h>
 
 // the ordering in the following
 // table should match the counter
@@ -53,14 +54,14 @@ void mtperf_report_diffs(const counter_t* before, const counter_t* after, int fl
 	if (i != 0) output_char(spec_sep ? sep : ',', stream);
 	output_int(after[i]-before[i], stream);
       }
-      output_char('\n', stream);
+      output_char((fflags & REPORT_NOLF) ? ' ' : '\n', stream);
     }
     break;
   case 1:
     // output raw
     for (i = 0; i < MTPERF_NCOUNTERS; ++i) {
       output_int(after[i]-before[i], stream);
-      output_char('\n', stream);
+      output_char((fflags & REPORT_NOLF) ? ' ' : '\n', stream);
     }
     break;
   case 2:
@@ -68,14 +69,82 @@ void mtperf_report_diffs(const counter_t* before, const counter_t* after, int fl
     output_char('[', stream);
     output_char('0', stream);
     output_char(',', stream);
-    output_int(MTPERF_NCOUNTERS, stream);
+    output_int(MTPERF_NCOUNTERS-1, stream);
     output_char(':', stream);
     for (i = 0; i < MTPERF_NCOUNTERS; ++i) {
       output_char(' ', stream);
       output_int(after[i]-before[i], stream);
     }
     output_char(']', stream);
-    output_char('\n', stream);
+    output_char((fflags & REPORT_NOLF) ? ' ' : '\n', stream);
     break;
   }
 }
+
+void mtperf_report_intervals(const struct s_interval* ivs,
+			     size_t n,
+			     int flags)
+{
+  size_t i;
+
+  int format = (flags >> 8) & 0xff;
+  int stream = (flags >> 24) & 0xff;
+  if (!stream) stream = 1;
+
+  if (format != 0)
+    output_string("### begin intervals\n", stream);
+
+  for (i = 0; i < n; ++i)
+    {
+      mtperf_report_diffs(ivs[i].before, ivs[i].after, flags | REPORT_NOLF);
+      output_string(" #", stream);
+      if (ivs[i].num >= 0) {
+	output_char(' ', stream);
+	output_uint(ivs[i].num, stream);
+      }
+      if (ivs[i].tag) {
+	output_char(' ', stream);
+	output_string(ivs[i].tag, stream);
+      }
+      if (!(flags & REPORT_NOLF))
+	output_char('\n', stream);
+      flags &= ~CSV_INCLUDE_HEADER;
+    }
+
+  if (format != 0)
+    output_string("### end intervals\n", stream);
+}
+
+struct s_interval* mtperf_alloc_intervals(size_t n) 
+{ 
+  return (struct s_interval*)fast_calloc(n, sizeof(struct s_interval)); 
+}
+
+void mtperf_free_intervals(struct s_interval* ivs)
+{
+  free(ivs);
+}
+
+#undef mtperf_start_interval
+void mtperf_start_interval(struct s_interval* ivs, size_t p,
+			   int numarg,
+			   const char *tag)
+{
+  mtperf_start_interval_(ivs, p, numarg, tag);
+}
+
+#undef mtperf_empty_interval
+void mtperf_empty_interval(struct s_interval* ivs, size_t p,
+			   int numarg,
+			   const char *tag)
+{
+  mtperf_empty_interval_(ivs, p, numarg, tag);
+}
+
+#undef mtperf_finish_interval
+void mtperf_finish_interval(struct s_interval* ivs, size_t p)
+{
+  mtperf_finish_interval_(ivs, p);
+}
+
+
