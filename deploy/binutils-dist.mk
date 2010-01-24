9cc14528bc8792c1da1f95b9825e9e723c4cb69c
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-01-24 17:30:45 +0000

    [sl-base] Fix packaging of binutils.
    * deploy/versions.mk.defaults,
    * deploy/binutils-dist.mk: Also use branch name.
    
    * deploy/rules/binutils.mk: Create directories later.

--- deploy/binutils-dist.mk
+++ deploy/binutils-dist.mk
@@ -5,21 +5,24 @@
 include common.mk
 include dist-common.mk
 
+BINUTILS_DIST_SVNBRANCH = trunk
 BINUTILS_DIST_REV = latest
 BINUTILS_DIST_BASEVER = 1.18
 
+BINUTILS_BASEBRANCH := $(strip $(shell basename $(BINUTILS_DIST_SVNBRANCH)))
 BU_SVN_REV := $(strip $(if $(filter latest, $(BINUTILS_DIST_REV)), \
-	          $(shell $(SVN) info $(BINUTILS_REPO) | \
+	          $(shell $(SVN) info $(BINUTILS_REPO)/$(BINUTILS_DIST_SVNBRANCH) | \
 	                  grep 'Last Changed Rev' | \
 	                  cut -d: -f2), $(BINUTILS_DIST_REV)))
 
-BINUTILS_DIST_VERSION = $(BINUTILS_DIST_BASEVER)-$(BU_SVN_REV)
+BINUTILS_DIST_VERSION = $(BINUTILS_DIST_BASEVER)-$(BU_SVN_REV)-$(BINUTILS_BASEBRANCH)
 BINUTILS_DISTBASE = binutils-$(BINUTILS_DIST_VERSION)
 
 $(META_SOURCES)/$(BINUTILS_DISTBASE)/download_done:
 	rm -f $@
 	mkdir -p $(META_SOURCES)
-	cd $(META_SOURCES) && $(SVN) export -r$(BU_SVN_REV) $(BINUTILS_REPO) $(BINUTILS_DISTBASE)
+	cd $(META_SOURCES) && \
+	   $(SVN) export -r$(BU_SVN_REV) $(BINUTILS_REPO)/$(BINUTILS_DIST_SVNBRANCH) $(BINUTILS_DISTBASE)
 	touch $@
 
 $(DISTDIR)/$(BINUTILS_DISTBASE).tar.bz2: $(META_SOURCES)/$(BINUTILS_DISTBASE)/download_done
--- deploy/rules/binutils.mk
+++ deploy/rules/binutils.mk
@@ -14,14 +14,14 @@ binutils-build: $(BINUTILS_BUILD_TARGETS)
 binutils-install: $(BINUTILS_INST_TARGETS)
 
 $(BINUTILS_SRC)/configure: $(BINUTILS_ARCHIVE)
-	mkdir -p $(SOURCES)
 	rm -f $@
+	mkdir -p $(SOURCES)
 	tar -C $(SOURCES) -xjvf $(BINUTILS_ARCHIVE)
 	touch $@
 
 $(BUILD)/binutils-$(BINUTILS_VERSION)-%/configure_done: $(BINUTILS_SRC)/configure
-	mkdir -p $(BUILD)/binutils-$(BINUTILS_VERSION)-$*
 	rm -f $@
+	mkdir -p $(BUILD)/binutils-$(BINUTILS_VERSION)-$*
 	SRC=$$(cd $(BINUTILS_SRC) && pwd) && \
            cd $(BUILD)/binutils-$(BINUTILS_VERSION)-$* && \
 	   $$SRC/configure --target=$* \
@@ -35,5 +35,5 @@ $(BUILD)/binutils-$(BINUTILS_VERSION)-%/gas/as-new: $(BUILD)/binutils-$(BINUTILS
 	cd $(BUILD)/binutils-$(BINUTILS_VERSION)-$* && $(MAKE) $(MAKE_FLAGS)
 
 $(REQDIR)/bin/%-as: $(BUILD)/binutils-$(BINUTILS_VERSION)-%/gas/as-new
-	cd $(BUILD)/binutils-$(BINUTILS_VERSION)-$* && $(MAKE) install
+	cd $(BUILD)/binutils-$(BINUTILS_VERSION)-$* && $(MAKE) -j1 install
 
