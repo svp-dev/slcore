387fbc627003af1cc68d798bee131e75e8618b42
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-01-26 15:13:41 +0000

    [sl-core] Extend the SL standard library.
    * lib/include/cstdarg.h: New.
    * lib/Makefile.am: Update accordingly.

---
+++ slc/lib/include/cstdarg.h
@@ -0,0 +1,34 @@
+//
+// cstdarg.h: this file is part of the SL toolchain.
+//
+// Copyright (C) 2010 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+#ifndef SLC_CSTDARG_H
+#define SLC_CSTDARG_H
+
+#ifdef __mt_freestanding__
+
+/* assume GCC support */
+typedef __builtin_va_list va_list;
+#define va_start(v,l)   __builtin_va_start(v,l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v,l)     __builtin_va_arg(v,l)
+#define va_copy(d,s)    __builtin_va_copy(d,s)
+
+#else
+#ifdef __cplusplus
+#include <cstdarg>
+#else
+#include <stdarg.h>
+#endif
+#endif
+
+#endif
