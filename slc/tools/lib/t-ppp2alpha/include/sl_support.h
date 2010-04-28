45a3bdd7fd963c23e1075ad5cdec1f3258909162
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-04-28 23:50:43 +0000

    [sl-core] Implement code generation for new back-end `ppp2'.
    * tools/bin/cce.in: Enable `-fwesley' by default for target
    `ppp2'.
    
    * tools/lib/t-ppp2alpha/slimpl.m4,
    * tools/lib/t-ppp2alpha/include/sl_support.h,
    * tools/bin/spp: Implement.

--- slc/tools/lib/t-ppp2alpha/include/sl_support.h
+++ slc/tools/lib/t-ppp2alpha/include/sl_support.h
@@ -85,7 +85,7 @@
 [[#]]define __sl_getsha(Name) __sl_sharg_ ## Name
 [[#]]define __sl_setsha(Name, Val) do {				\
   __typeof__(Val) __tmp_set_ ## Name = (Val);			\
-  __asm__ __volatile__("# MT: writing shared " # Name " (%0)"	\
+  __asm__ ("puts %0, %1, %2 # MT: writing shared " # Name " (%0)"	\
 		       : "=rf"(__sl_arg_ ## Name)		\
 		       : "0"(__sl_arg_ ## Name));		\
   __sl_arg_ ## Name = __tmp_set_ ## Name;			\
@@ -107,31 +107,66 @@
 [[#]]define __sl_epilogue(Name, ArgSpec)		\
   __sl_end_thread: (void)0; }
 
-[[#]]define __sl_allocate(Tag, Place, GI, SI, GF, SF)			\
+[[#]]define __sl_allocate(Tag, Place)			\
   register long __sl_fid_ ## Tag;					\
-  __asm__ __volatile__ ("allocate %0, %2, %3, %4, %5\t# CREATE" #Tag	\
+  __asm__ ("allocate %1, %0\t# MT: CREATE" #Tag	\
 			: "=r"(__sl_fid_ ## Tag)			\
-			: "0"(Place), "I"(GI), "I"(SI), "I"(GF), "I"(SF))
+			: "rI"(Place))
 
 [[#]]define __sl_setstart(Tag, Start) \
-  __asm__ ("setstart %0, %2\t# CREATE " #Tag		\
+  __asm__ ("setstart %0, %2\t# MT: CREATE " #Tag		\
 	   : "=r"(__sl_fid_ ## Tag)			\
 	   : "0"(__sl_fid_ ## Tag), "rI"(Start))
 
 [[#]]define __sl_setlimit(Tag, Limit) \
-  __asm__ ("setlimit %0, %2\t# CREATE " #Tag \
+  __asm__ ("setlimit %0, %2\t# MT: CREATE " #Tag \
 	   : "=r"(__sl_fid_ ## Tag) \
 	   : "0"(__sl_fid_ ## Tag), "rI"(Limit))
 
 [[#]]define __sl_setstep(Tag, Step) \
-  __asm__ ("setstep %0, %2\t# CREATE " #Tag \
+  __asm__ ("setstep %0, %2\t# MT: CREATE " #Tag \
 	   : "=r"(__sl_fid_ ## Tag) \
 	   : "0"(__sl_fid_ ## Tag), "rI"(Step))
 
 [[#]]define __sl_setblock(Tag, Block)					\
-  __asm__ ("setblock %0, %2\t# CREATE " #Tag \
+  __asm__ ("setblock %0, %2\t# MT: CREATE " #Tag \
 	   : "=r"(__sl_fid_ ## Tag) \
 	   : "0"(__sl_fid_ ## Tag), "rI"(Block))
+
+[[#]]define __sl_setshfa(Name, Tag, Val, Offset) \
+  __asm__ ("fputs %2, %0, " Offset "\t# MT: set shared " # Name : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "f"(Val))
+
+[[#]]define __sl_setshia(Name, Tag, Val, Offset) \
+  __asm__ ("puts %2, %0, " Offset "\t# MT: set shared " # Name : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "rI"(Val))
+
+[[#]]define __sl_setma(Tag, Offset) \
+  __asm__ ("wb; putg %2, %0, " Offset "\t# MT: set offset for memargs" : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "r"(&__sl_ma_data_ ## Tag))
+
+[[#]]define __sl_setglfa(Name, Tag, Val, Offset) \
+  __asm__ ("fputg %2, %0, " Offset "\t# MT: set global " # Name : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "f"(__sl_after_ ## Name = (Val)))
+
+[[#]]define __sl_setglia(Name, Tag, Val, Offset) \
+  __asm__ ("putg %2, %0, " Offset "\t# MT: set global " # Name : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "rI"(__sl_after_ ## Name = (Val)))
+
+[[#]]define __sl_getshfa(Name, Tag, Offset) \
+  __asm__ ("fgets %0, " Offset ", %1\t# MT: get shared " # Name : "=r"(__sl_fid_ ## Tag), "=f"(__sl_after_ ## Name) : "0"(__sl_fid_ ## Tag))
+
+[[#]]define __sl_getshia(Name, Tag, Offset) \
+  __asm__ ("gets %0, " Offset ", %1\t# MT: get shared " # Name : "=r"(__sl_fid_ ## Tag), "=r"(__sl_after_ ## Name) : "0"(__sl_fid_ ## Tag))
+
+[[#]]define __sl_create(Tag, OffsetGP)                                         \
+  __asm__ __volatile__("crei %0, 0(%2); wb; putg %2, %0, " OffsetGP "\t# MT: CREATE " # Tag : "=r"(__sl_fid_ ## Tag) : "0"(__sl_fid_ ## Tag), "r"(__sl_funcptr_ ## Tag) : "memory")
+
+[[#]]define __sl_sync_nouse(Tag)                                       \
+  __asm__("sync %0, %1; mov %1, $31\t# MT: SYNC " # Tag : "=r"(__sl_fid_ ## Tag), "=r"(__sl_syncvar_ ## Tag) : "0"(__sl_fid_ ## Tag) : "memory")
+
+[[#]]define __sl_sync(Tag, Var)                                           \
+  __asm__("sync %0, %1; mov %1, %1\t# MT: SYNC " # Tag : "=r"(__sl_fid_ ## Tag), "=r"(Var) : "0"(__sl_fid_ ## Tag) : "memory")
+
+[[#]]define __sl_release(Tag) \
+  __asm__ __volatile__("release %0" : : "r"(__sl_fid_ ## Tag));
+
+
 
 [[#]]define __sl_dobreak(Val) do {			    \
     __asm__ __volatile__("break %0" : : "rI"(Val)); \
@@ -159,6 +194,9 @@
 [[#]]define __sl_declgla_noreg(Type, Name, Init)		\
   register Type const __sl_arg_ ## Name = (Init)
 
+[[#]]define __sl_declgla_noreg_undef(Type, Name)		\
+  register Type __sl_arg_ ## Name
+
 [[#]]define __sl_declgla(Type, Name, Reg, Init)			\
   register Type const __sl_arg_ ## Name __asm__(Reg) = (Init)
 
@@ -183,9 +221,6 @@
 
 [[#]]define __sl_declbr(Tag, Type, Reg)		\
   register Type const __sl_br_ ## Tag __asm__(Reg)
-
-[[#]]define __sl_declsync(Tag, Reg) \
-  register long __sl_sync_ ## Tag __asm__(Reg) = __sl_fid_ ## Tag
 
 extern void _sl_callgate(void);
 
