708d5fd51d2fee989510b2408eb2bbe1618a0f7f
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-07-02 15:10:03 +0000

    [sl-tests] Extend the test suite.
    * 0_slc/0_trivial/ret.c: New.

---
+++ tests/0_slc/0_trivial/ret.c
@@ -0,0 +1,23 @@
+//
+// ret.c: this file is part of the SL toolchain.
+//
+// Copyright (C) 2010 The SL project.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// The complete GNU General Public Licence Notice can be found as the
+// `COPYING' file in the root directory.
+//
+
+#include <svp/testoutput.h>
+
+sl_def(t_main, void)
+{
+    output_string("hello\n", 1);
+    return ;
+    output_string("world\n", 1);
+}
+sl_enddef
