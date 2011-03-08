9bba8425ebb9a40a0a7c5112d6ac35a44950a3aa
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-03-08 14:52:21 +0100

    [sl-base] Split programs, demos and benchmarks to a separate repository.
    * programs/: Remove. This is now hosted elsewhere.
    (https://git.svp-home.org/slprogs.git)
    
    * bootstrap,
    * configure.ac,
    * Makefile.am: Update accordingly.

--- programs/benchmarks/ca/benchca_gfx.c
+++
@@ -1,4 +0,0 @@
-#define DISPLAY_DURING_COMPUTE 1
-#define CENTER_INIT_PATTERN 1
-#include "benchca.c"
-
--- programs/benchmarks/game_of_life/worker.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORKER
-#define WORKER
-
-//sl_decl(run, void, sl_glparm(int, iteration), sl_glparm(int, N));
-sl_decl(run, void, sl_glparm(int, iteration),sl_shparm(int,sta));
-//void run();
-
-#endif 
