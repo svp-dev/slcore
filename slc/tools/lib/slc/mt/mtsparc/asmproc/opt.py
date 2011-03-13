736383e0e6447b843fb1993c7bf8594d96f9a5bb
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-03-13 13:12:07 +0100

    [sl-core] Initialize the MT-Sparc assembly optimization stage.
    * tools/lib/slc/mt/mtsparc/asmproc/opt.py (filter): Use the common
    enhance code.

--- slc/tools/lib/slc/mt/mtsparc/asmproc/opt.py
+++ slc/tools/lib/slc/mt/mtsparc/asmproc/opt.py
@@ -1,4 +1,13 @@
+from common import *
+
+_filter_begin = [reader, lexer, splitsemi, parser]
+_filter_stages = []
+_filter_end = [printer]
+                 
+
+
+from ...common.asmproc import enhance
 
 def filter(*args):
-	pass
+	return enhance.filter(_filter_begin, _filter_stages, _filter_end, *args)
 
