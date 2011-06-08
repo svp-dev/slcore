4d332062968906b224a66aa44813c3403e0e6b6d
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-06-08 12:23:19 +0200

    [sl-core] Extend the behavior of `slr -n'.
    To prevent result variations due to placement, it is useful to
    allow various values of `-n' to place the execution from the same
    first core. For this a new environment variable `SLR_PLACESIZE' is
    introduced. When this is set, its value is used to allocate a
    place and the value of `-n' (`SLR_NCORES') is then use to adjuste
    the size field within the allocated place identifier.
    
    * lib/src/mtinit.c (sys_check_ncores): Do it.

--- slc/lib/src/mtinit.c
+++ slc/lib/src/mtinit.c
@@ -101,10 +101,13 @@ void sys_environ_init(char *initenv)
 extern sl_place_t __main_place_id;
 
 static noinline
-void sys_set_main_pid(unsigned req_ncores)
+void sys_set_main_pid(unsigned req_ncores, unsigned req_placesize)
 {
     if (verbose_boot) output_string(" -> request to SEP...", 2);
-    int r = sep_alloc(root_sep, &__main_place_id, SAL_EXACT, req_ncores);
+
+    sl_place_t p;
+
+    int r = sep_alloc(root_sep, &p, SAL_EXACT, req_placesize);
     if (r == -1) 
     {
         if (verbose_boot)
@@ -113,8 +116,13 @@ void sys_set_main_pid(unsigned req_ncores)
     }
     if (verbose_boot) 
     {
+        p = p & (p - 1); // drop the existing size bit
+        p |= req_ncores; // add the new size bit
+
+        __main_place_id = p;
+
         output_string(" success, t_main goes to pid=0x", 2);
-        output_hex(__main_place_id, 2);
+        output_hex(p, 2);
         output_ts(2);
         output_char('\n', 2);
     }
@@ -124,14 +132,24 @@ static noinline
 void sys_check_ncores(void)
 {
     char *v = getenv("SLR_NCORES");
+    char *v2 = getenv("SLR_PLACESIZE");
     if (v && v[0]) {
         if (verbose_boot) {
             output_string("* -n ", 2);
             output_string(v, 2);
         }
         unsigned req_ncores = strtoul(v, 0, 10);
-        if (req_ncores)
-            sys_set_main_pid(req_ncores);
+        if (req_ncores && ((req_ncores & (req_ncores - 1)) == 0))
+        {
+            unsigned req_placesize = 0;
+            if (v2 && v2[0])
+                req_placesize = strtoul(v2, 0, 10);
+            req_placesize = req_placesize ? req_placesize : req_ncores;
+            if (req_ncores > req_placesize)
+                req_ncores = req_placesize;
+
+            sys_set_main_pid(req_ncores, req_placesize);
+        }
         else
             if (verbose_boot) 
                 output_string(" (invalid, ignoring)\n", 2);
