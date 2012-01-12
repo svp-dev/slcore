bcb13c8537fba5be26f84d2933bf8a6234bfe383
Raphael 'kena' Poss <r.c.poss@uva.nl>
2012-01-12 11:21:20 +0100

    [sl-core] Enhance code documentation.
    * lib/mgos/posix-compat/gtod_virtual.c (gettimeofday_virtual):
    Explain the ratio between core ticks and mgconf frequency.

--- slc/lib/mgos/posix-compat/gtod_virtual.c
+++ slc/lib/mgos/posix-compat/gtod_virtual.c
@@ -16,6 +16,8 @@ int gettimeofday_virtual(struct timeval* tv, struct timezone *tz)
     clock_t ticks = clock();
     unsigned long freq = (mgconf_core_freq == (confword_t)-1) ? 1000 : mgconf_core_freq;
 
+    /* the mgsim frequencies are expressed in MHz. So it is a direct
+     factor between clock ticks and microseconds. */
     unsigned long long usecs = ticks / (unsigned long long)freq;
     unsigned long secs = usecs / 1000000;
 
