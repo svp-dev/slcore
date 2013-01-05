691f765000ccb72cdc37156f3e084b9e37b82474
Raphael 'kena' Poss <r.poss@uva.nl>
2013-01-05 22:26:07 +0100

    [sl-core] Provide more compatibility with MT-SPARC's single-precision FP support.
    * lib/libc/misc/dtoa_simple.c (dtoa_simple): Avoid using large
    exponents when using short doubles.

--- slc/lib/libc/misc/dtoa_simple.c
+++ slc/lib/libc/misc/dtoa_simple.c
@@ -15,7 +15,12 @@ char *dtoa_simple(double x, int mode, int _prec, int *decpt, int *sign, char **r
 
     if (*sign)
         x = -x;
-    if (x < __DBL_MIN__) 
+
+#ifdef __slc_short_double__
+    if (x < __FLT_MIN__)
+#else
+    if (x < __DBL_MIN__)
+#endif
     {
         *decpt = 1;
         return nrv_alloc("0", rve, 1);
@@ -23,18 +28,20 @@ char *dtoa_simple(double x, int mode, int _prec, int *decpt, int *sign, char **r
 
     /* -- find exponent and normalize -- */
     int exp = 1;
+#ifndef __slc_short_double__
+    // Cannot divide by 1e100 with 32-bit floats
     while (x >= 1e100) { x /= 1e100; exp += 100; }
+#endif
     while (x >= 1e10) { x /= 1e10; exp += 10; }
     while (x >= 10.) { x /= 10.; ++exp; }
+#ifndef __slc_short_double
     while (x && x < 1e-100) { x *= 1e100; exp -= 100; }
+#endif
     while (x && x < 1e-10) { x *= 1e10; exp -= 10; }
     while (x && x < 1.0) { x *= 10.; --exp; }
 
     int k = exp;
     int prec = _prec;
-    //output_string("req mode ", 1); output_int(mode, 1);
-    //output_string(" prec ", 1); output_int(prec, 1);
-    //output_string(" exp ", 1); output_int(exp, 1);
     if (mode == 2 && prec <= 0)
         prec = 1;
     else if (mode == 3) {
@@ -42,9 +49,6 @@ char *dtoa_simple(double x, int mode, int _prec, int *decpt, int *sign, char **r
         if (prec <= 0) { k = -_prec; prec = 1; }
     }
     if (prec > 15) prec = 15;
-    //output_string(" -> prec ", 1); output_int(prec, 1);
-    //output_string(" exp ", 1); output_int(*decpt, 1);
-    //output_char('\n', 1);
 
     char *buf = rv_alloc(prec);
     char *p = buf;
@@ -54,7 +58,6 @@ char *dtoa_simple(double x, int mode, int _prec, int *decpt, int *sign, char **r
         d = (unsigned long)x;
         *p++ = d + '0';
         x = (x - d) * 10.;
-        //output_char('.', 1);
     };
 
     unsigned long last = (unsigned long) x;
@@ -77,9 +80,6 @@ ret:
     *p = 0;
 
     if (rve) *rve = p;
-    //output_string("gen -> ::", 1);
-    //output_string(buf, 1);
-    //output_string("::\n", 1);
     return buf;
 }
 
