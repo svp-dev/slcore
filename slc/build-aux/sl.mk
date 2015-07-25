ed9b23755fee79e70f4bd55b1cfdfd639f51fbd3
Raphael 'kena' Poss <knz@thaumogen.net>
2015-07-25 22:42:02 +0200

    [sl-core] Enhance the usability of derived packages.
    * build-aux/sl.mk: Simplify the target generation rules. This also
    enables parallel builds of different targets.

--- slc/build-aux/sl.mk
+++ slc/build-aux/sl.mk
@@ -1,6 +1,6 @@
 ## sl.mk: this file is part of the SL toolchain.
-## 
-## Copyright (C) 2009,2010 The SL project
+##
+## Copyright (C) 2009-2015 The SL project
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License
@@ -13,7 +13,7 @@
 
 include $(top_srcdir)/build-aux/slcvars.mk
 
-SUFFIXES = .x
+SUFFIXES = .x .bin
 
 slc_verbose = $(slc_verbose_$(V))
 slc_verbose_ = $(slc_verbose_$(AM_DEFAULT_VERBOSITY))
@@ -23,61 +23,37 @@ slc_shverbose = $(slc_shverbose_$(V))
 slc_shverbose_ = $(slc_shverbose_$(AM_DEFAULT_VERBOSITY))
 slc_shverbose_0 = :
 
+SL_TARGET_LIST =
+
 if ENABLE_SLC_MTALPHA
-slc_ifmtalpha =
-else
-slc_ifmtalpha = :
+SL_TARGET_LIST += mta mta_s mta_n mta_on
 endif
 
 if ENABLE_SLC_MTSPARC
-slc_ifmtsparc = 
-else
-slc_ifmtsparc = :
+SL_TARGET_LIST += mtsn mtsn_s mtsn_n
+endif
+
+if ENABLE_SLC_MIPSEL
+SL_TARGET_LIST += mipsel_s
 endif
 
 if ENABLE_SLC_PTL
-slc_ifptl =
-else
-slc_ifptl = :
+SL_TARGET_LIST += ptl_n ptl_s ptld_n ptld_s
 endif
 
-SLC_BEFORE = function slc_compile() { \
-	$(slc_shverbose) set -x ; \
-	if test -n "$$SLC_OUT"; then rm -f "$$SLC_OUT" "$$SLC_OUT".{mtalpha,ptl,seqc}; fi && \
-	echo "  SLC    $$SLC_OUT".seqc && \
-	$(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".seqc} -b seqc "$$@" -I$(srcdir) -I$(builddir) \
-	      $(AM_CFLAGS) $(CFLAGS) $(SLFLAGS) $(SLFLAGS_SEQC) $(LIBS) && \
-	$(slc_ifptl) echo "  SLC    $$SLC_OUT".ptl && \
-        $(slc_ifptl) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".ptl} -b ptl "$$@" -I$(srcdir) -I$(builddir) \
-	      $(AM_CXXFLAGS) $(CXXFLAGS) $(SLFLAGS) $(SLFLAGS_PTL) $(LIBS) && \
-	$(slc_ifmtalpha) echo "  SLC    $$SLC_OUT".mta && \
-        $(slc_ifmtalpha) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mta} -b mta "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTALPHA) $(SLFLAGS_MTA) && \
-	$(slc_ifmtalpha) echo "  SLC    $$SLC_OUT".mta_n && \
-        $(slc_ifmtalpha) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mta_n} -b mta_n "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTALPHA) $(SLFLAGS_MTA_N) && \
-	$(slc_ifmtalpha) echo "  SLC    $$SLC_OUT".mta_on && \
-        $(slc_ifmtalpha) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mta_on} -b mta_on "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTALPHA) $(SLFLAGS_MTA_ON) && \
-	$(slc_ifmtalpha) echo "  SLC    $$SLC_OUT".mta_s && \
-        $(slc_ifmtalpha) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mta_s} -b mta_s "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTALPHA) $(SLFLAGS_MTA_S) && \
-	$(slc_ifmtsparc) echo "  SLC    $$SLC_OUT".mtsn_n && \
-        $(slc_ifmtsparc) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mtsn_n} -b mtsn_n "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTSPARC) $(SLFLAGS_MTS_N) && \
-	$(slc_ifmtsparc) echo "  SLC    $$SLC_OUT".mtsn_s && \
-        $(slc_ifmtsparc) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mtsn_s} -b mtsn_s "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTSPARC) $(SLFLAGS_MTS_S) && \
-	$(slc_ifmtsparc) echo "  SLC    $$SLC_OUT".mtsn && \
-        $(slc_ifmtsparc) $(SLC_LOCAL) $${SLC_OUT:+-o "$$SLC_OUT".mtsn} -b mtsn "$$@" \
-	      -I$(srcdir) -I$(builddir) -lm $(SLFLAGS) $(SLFLAGS_MTSPARC) $(SLFLAGS_MTS) && \
-	if test -n "$$SLC_OUT"; then \
-	  printf '\#! /bin/sh\n' >"$@" && \
-	  printf 'echo "This script is only a stub. Run the actual program with:"\n' >>"$@" && \
-	  printf 'echo; echo "\tslr "$$(basename "$$0" .x)".bin.XXXX"; echo; exit 1\n' >>"$@"; \
-	fi; }
+if ENABLE_SLC_HLSIM
+SL_TARGET_LIST += hls hls_n hls_s hlsd hlsd_n hlsd_s
+endif
 
-.c.x:
-	@$(SLC_BEFORE); $(SLC_VARS) SLC_OUT="$(@:.x=.bin)" slc_compile $<
+.PHONY: %.bin
+%.bin:
+	$(slc_verbose)t=$@; t=$${t%.bin}; t=$${t##*.}; \
+	 cmd="$(SLC_LOCAL) -b $$t -o $@ $(SOURCE) -I$(srcdir) -I$(builddir) $(SLFLAGS) `eval echo \\$$SLFLAGS_$$t`"; \
+	 $(slc_shverbose) echo "$$cmd"; \
+	 $$cmd
 
-
+%.x: %.c
+	$(AM_V_at)rm -f $@ $*.*.bin
+	$(AM_V_at)$(foreach P,$(SL_TARGET_LIST), export SLFLAGS_$(P)="$(SLFLAGS_$(P))" &&) \
+	$(MAKE) `n=$@; for t in $(SL_TARGET_LIST); do echo $${n%.x}.$$t.bin; done` SOURCE=$<
+	$(AM_V_at)echo echo "This script is only a stub, run .bin with slr." >$@
