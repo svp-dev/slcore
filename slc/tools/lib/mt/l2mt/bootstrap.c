9572860396ded3214d975d11e0a7e740fdec484a
Raphael 'kena' Poss <knz@thaumogen.net>
2016-09-13 16:30:40 +0200

    [sl-core] Fix a bootstrap issue on LEON2-MT.
    * tools/lib/mt/l2mt/bootstrap.c: Avoid copying if the data already
    sits in the right place

--- slc/tools/lib/mt/l2mt/bootstrap.c
+++ slc/tools/lib/mt/l2mt/bootstrap.c
@@ -1,10 +1,12 @@
 extern unsigned _edata;
 extern unsigned _data_start;
 extern unsigned _endtext;
+extern unsigned _RAM_START;
 
 void __copyram(void) {
     unsigned *src = &_endtext;
     unsigned *dst = &_data_start;
-    while (dst != &_edata)
-	*dst++ = *src++;
+    if (src < &_RAM_START)
+	while (dst != &_edata)
+	    *dst++ = *src++;
 }
